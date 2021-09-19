c5c4ff6cff8d92ed0136fe7ae6e2385d08e1e026
Максим Глушков <61595112+maxglushkov@users.noreply.github.com>
2021-09-19 16:10:49 +0300

    Слежение за ветром

--- lab02/WeatherStation.5/WeatherData.h
+++ lab02/WeatherStation.5/WeatherData.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <cmath>
 #include "Observer.h"
 
 struct SWeatherInfo
@@ -10,6 +11,8 @@ struct SWeatherInfo
 	double temperature = 0;
 	double humidity = 0;
 	double pressure = 0;
+	double windSpeed = 0;
+	double windDirection = 0;
 };
 
 class CDisplay: public IObserver<SWeatherInfo>
@@ -24,6 +27,8 @@ private:
 		std::cout << "Current Temp " << data.temperature << std::endl;
 		std::cout << "Current Hum " << data.humidity << std::endl;
 		std::cout << "Current Pressure " << data.pressure << std::endl;
+		std::cout << "Current Wind Speed " << data.windSpeed << std::endl;
+		std::cout << "Current Wind Direction " << data.windDirection << std::endl;
 		std::cout << "----------------" << std::endl;
 	}
 };
@@ -37,28 +42,63 @@ private:
 	*/
 	void Update(SWeatherInfo const& data) override
 	{
-		if (m_minTemperature > data.temperature)
-		{
-			m_minTemperature = data.temperature;
-		}
-		if (m_maxTemperature < data.temperature)
-		{
-			m_maxTemperature = data.temperature;
-		}
-		m_accTemperature += data.temperature;
-		++m_countAcc;
-
-		std::cout << "Max Temp " << m_maxTemperature << std::endl;
-		std::cout << "Min Temp " << m_minTemperature << std::endl;
-		std::cout << "Average Temp " << (m_accTemperature / m_countAcc) << std::endl;
-		std::cout << "----------------" << std::endl;
+		m_temperature.Update("Temp", data.temperature);
+		m_humidity.Update("Hum", data.humidity);
+		m_pressure.Update("Pressure", data.pressure);
+		m_windSpeed.Update("Wind Speed", data.windSpeed);
+		m_windDirection.Update("Wind Direction", data.windSpeed, data.windDirection);
 	}
 
-	double m_minTemperature = std::numeric_limits<double>::infinity();
-	double m_maxTemperature = -std::numeric_limits<double>::infinity();
-	double m_accTemperature = 0;
-	unsigned m_countAcc = 0;
+	class CGenericStats
+	{
+	public:
+		void Update(std::string const& name, double data)
+		{
+			if (m_min > data)
+			{
+				m_min = data;
+			}
+			if (m_max < data)
+			{
+				m_max = data;
+			}
+			m_acc += data;
+			++m_count;
 
+			std::cout << "Max " << name << ' ' << m_max << std::endl;
+			std::cout << "Min " << name << ' ' << m_min << std::endl;
+			std::cout << "Average " << name << ' ' << (m_acc / m_count) << std::endl;
+			std::cout << "----------------" << std::endl;
+		}
+	private:
+		double m_min = std::numeric_limits<double>::infinity();
+		double m_max = -std::numeric_limits<double>::infinity();
+		double m_acc = 0;
+		unsigned m_count = 0;
+	}
+	m_temperature, m_humidity, m_pressure, m_windSpeed;
+
+	class CPolarAngleStats
+	{
+	public:
+		void Update(std::string const& name, double radius, double angle)
+		{
+			angle *= M_PI / 180.0;
+			m_accX += radius * cos(angle);
+			m_accY += radius * sin(angle);
+
+			angle = atan2(m_accY, m_accX) / M_PI * 180.0;
+			if (angle < 0)
+			{
+				angle += 360.0;
+			}
+			std::cout << "Average " << name << ' ' << angle << std::endl;
+			std::cout << "----------------" << std::endl;
+		}
+	private:
+		double m_accX = 0, m_accY = 0;
+	}
+	m_windDirection;
 };
 
 class CWeatherData : public CObservable<SWeatherInfo>
@@ -79,17 +119,29 @@ public:
 	{
 		return m_pressure;
 	}
+	// Скорость ветра (в м/с)
+	double GetWindSpeed()const
+	{
+		return m_windSpeed;
+	}
+	// Направление ветра (в градусах, где 0 – север, 90-восток, 180-юг, 270-запад)
+	double GetWindDirection()const
+	{
+		return m_windDirection;
+	}
 
 	void MeasurementsChanged()
 	{
 		NotifyObservers();
 	}
 
-	void SetMeasurements(double temp, double humidity, double pressure)
+	void SetMeasurements(double temp, double humidity, double pressure, double windSpeed, double windDirection)
 	{
 		m_humidity = humidity;
 		m_temperature = temp;
 		m_pressure = pressure;
+		m_windSpeed = windSpeed;
+		m_windDirection = windDirection;
 
 		MeasurementsChanged();
 	}
@@ -100,10 +152,14 @@ protected:
 		info.temperature = GetTemperature();
 		info.humidity = GetHumidity();
 		info.pressure = GetPressure();
+		info.windSpeed = GetWindSpeed();
+		info.windDirection = GetWindDirection();
 		return info;
 	}
 private:
 	double m_temperature = 0.0;
 	double m_humidity = 0.0;
 	double m_pressure = 760.0;
+	double m_windSpeed = 0.0;
+	double m_windDirection = 0.0;
 };
--- lab02/WeatherStation.5/main.cpp
+++ lab02/WeatherStation.5/main.cpp
@@ -10,12 +10,12 @@ int main()
 	CStatsDisplay statsDisplay;
 	wd.RegisterObserver(statsDisplay);
 
-	wd.SetMeasurements(3, 0.7, 760);
-	wd.SetMeasurements(4, 0.8, 761);
+	wd.SetMeasurements(3, 0.7, 760, 4, 90);
+	wd.SetMeasurements(4, 0.8, 761, 5, 270);
 
 	wd.RemoveObserver(statsDisplay);
 
-	wd.SetMeasurements(10, 0.8, 761);
-	wd.SetMeasurements(-10, 0.8, 761);
+	wd.SetMeasurements(10, 0.8, 761, 3, 180);
+	wd.SetMeasurements(-10, 0.8, 761, 0, 0);
 	return 0;
 }
